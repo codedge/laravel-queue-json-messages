9c135f10c0ab010cf11d4d957f16c33e996878dc
codedge <holger.loesken@codedge.de>
2020-04-24 13:21:44 +0000

    Apply fixes from StyleCI

--- src/QueueJsonMessageServiceProvider.php
+++ src/QueueJsonMessageServiceProvider.php
@@ -1,9 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Codedge\QueueJsonMessage;
 
 use Illuminate\Support\ServiceProvider;
-
 
 final class QueueJsonMessageServiceProvider extends ServiceProvider
 {
